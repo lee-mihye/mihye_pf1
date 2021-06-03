e0ea9d5a1c5edb18b477a2e50c31a477971870e2
lee-mihye <gnsap@naver.com>
2021-06-03 15:39:10 +0900

    og9

---
+++ js/scripts.js
@@ -0,0 +1,131 @@
+/*!
+ * Start Bootstrap - Freelancer v6.0.5 (https://startbootstrap.com/theme/freelancer)
+ * Copyright 2013-2020 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
+ */
+(function ($) {
+    "use strict"; // Start of use strict
+
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: (target.offset().top - 71)
+                }, 1000, "easeInOutExpo");
+                return false;
+            }
+        }
+    });
+
+    // Scroll to top button appear
+    $(document).scroll(function () {
+        var scrollDistance = $(this).scrollTop();
+        if (scrollDistance > 100) {
+            $('.scroll-to-top').fadeIn();
+        } else {
+            $('.scroll-to-top').fadeOut();
+        }
+    });
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    $('.js-scroll-trigger').click(function () {
+        $('.navbar-collapse').collapse('hide');
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $('body').scrollspy({
+        target: '#mainNav',
+        offset: 80
+    });
+
+    // Collapse Navbar
+    var navbarCollapse = function () {
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").addClass("navbar-shrink");
+        } else {
+            $("#mainNav").removeClass("navbar-shrink");
+        }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
+
+    // Floating label headings for the contact form
+    $(function () {
+        $("body").on("input propertychange", ".floating-label-form-group", function (e) {
+            $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+        }).on("focus", ".floating-label-form-group", function () {
+            $(this).addClass("floating-label-form-group-with-focus");
+        }).on("blur", ".floating-label-form-group", function () {
+            $(this).removeClass("floating-label-form-group-with-focus");
+        });
+    });
+
+
+
+
+    // 하단 이동 버튼
+    var winH = $(window).height();
+    $(".mouse").click(function () {
+        $("#page-top").animate({
+            scrollTop: winH + "px"
+        }, 800, "easeOutQuint", function () {
+            // 이동후 부드러운 스크롤 전역이동변수에 위치업데이트
+            currentY = -winH;
+        }); //// animate /////////
+    }); ////////// click /////////////
+
+
+    //////////////// 문자 타이핑//////////////////
+
+    var typingBool = false;
+    var typingIdx = 0;
+    var liIndex = 0;
+    var liLength = $(".typing-txt>ul>li").length;
+
+    // 타이핑될 텍스트를 가져온다 
+    var typingTxt = $(".typing-txt>ul>li").eq(liIndex).text();
+    typingTxt = typingTxt.split(""); // 한글자씩 자른다. 
+    if (typingBool == false) { // 타이핑이 진행되지 않았다면 
+        typingBool = true;
+        var tyInt = setInterval(typing, 100); // 반복동작 
+    }
+
+    function typing() {
+        $(".typing ul li").removeClass("on");
+        $(".typing ul li").eq(liIndex).addClass("on");
+        if (typingIdx < typingTxt.length) { // 타이핑될 텍스트 길이만큼 반복 
+            $(".typing ul li").eq(liIndex).append(typingTxt[typingIdx]); // 한글자씩 이어준다. 
+            typingIdx++;
+        } else {
+            if (liIndex < liLength - 1) {
+                //다음문장으로  가기위해 인덱스를 1증가
+                liIndex++;
+                //다음문장을 타이핑하기위한 셋팅
+                typingIdx = 0;
+                typingBool = false;
+                typingTxt = $(".typing-txt>ul>li").eq(liIndex).text();
+
+                //다음문장 타이핑전 1초 쉰다
+                clearInterval(tyInt);
+                //타이핑종료
+
+                setTimeout(function () {
+                    //1초후에 다시 타이핑 반복 시작
+                    tyInt = setInterval(typing, 100);
+                }, 1000);
+            } else if (liIndex == liLength - 1) {
+
+                //마지막 문장까지 써지면 반복종료
+                clearInterval(tyInt);
+            }
+        }
+    }
+
+
+
+})(jQuery); // End of use strict
